7ffbab6840a9b2b9cf1291bfa1912b62b1290fe0
edwardalx <eobkwaku@gmail.com>
2025-02-14 14:58:11 +0000

    week9_v14

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -10,19 +10,25 @@ class Book(models.Model):
     def __str__(self):
         return f"title : {self.title} author: {self.author} publication_year:{self.publication_year} "
 
-newbook = Book('1984' , author='George Orwell', publication_year=1949)
 
-books = Book.objects.all()
-book = Book.objects.get(id = 1)
+# create_book = Book.objects.create(title = "1984", author = "George Orwell", publication_year = 1949)
+
+# retrieve_books = Book.objects.get(id = 5)
+
+# books = Book.objects.all()
+# book = Book.objects.get(id = 1)
 
 # update_title = Book.objects.get(id = 1)
 # update_title.title = "Edward"
 # update_title.save()
 # print(book)
 
+# book = Book.objects.get(id=1)
+# book.title = "Nineteen Eighty-Four" 
+# book.save()
 
-update_title = Book.objects.filter(author="George Orwell").update(title="Edward")
+# update_title = Book.objects.filter(author="George Orwell").update(title="Edward")
 
-book = Book.objects.get(id = 1)
-book.delete()
+# book = Book.objects.get(id = 1)
+# book.delete()
 
