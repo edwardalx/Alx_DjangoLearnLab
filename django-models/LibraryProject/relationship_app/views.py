6c5241fe61f37a62210681fac413e95e941d45c3
edwardalx <eobkwaku@gmail.com>
2025-02-24 00:11:07 +0000

    week10_v11.3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.http import HttpResponseRedirect
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
@@ -22,13 +24,31 @@ class LibraryDetailView(DetailView):
         library = self.object
         context['books'] = library.books.all()
         return context
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)  # Log the user in after registration
+#             return redirect('home')  # Redirect to the home page
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'registration/register.html', {'form': form})
+
+# Register view
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('/')  # Redirect to the home page
+            login(request, user)  # Automatically log the user in after registration
+            return redirect('home')  # Redirect to the homepage
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = 'registration/logout.html'
