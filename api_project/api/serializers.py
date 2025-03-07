03a041770968bab69ed4a6329990b48ab796a440
edwardalx <eobkwaku@gmail.com>
2025-03-07 00:30:31 +0000

    week12_v10

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,6 +1,8 @@
 from .models import Book
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
