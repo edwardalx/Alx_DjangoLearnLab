6eefbed7dd9b79f99afde82c946057baaf007ab6
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-13 17:52:52 +0000

    week13_fixed_main

--- advanced_api_project/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ListView,DetailView,CreateView,UpdateView,DeleteView
-urlpatterns = [
-    path('book/', view=ListView.as_view(), name='list-books' ),
-    path('book/<int:pk>/', view=DetailView.as_view(), name='retrieve-book' ),
-    path('book/create', view=CreateView.as_view(), name='create-books' ),
-    path('book/update/<int:pk>/', view=UpdateView.as_view(), name='update-book' ),
-    path('book/delete/<int:pk>/', view=DeleteView.as_view(), name='delete-book' ),
-]
