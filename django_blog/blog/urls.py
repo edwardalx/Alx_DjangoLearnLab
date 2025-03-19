0aae1d9954e3d33a0729b9a5c685fff66f5adea8
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-19 00:38:34 +0000

    week14_V4

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import Home, Register
+from .views import Home, Register,Profile
 from django.urls import path, include
 
 urlpatterns = [
     path('', view=Home.as_view(), name='home'),
     path('login/', view=LoginView.as_view(template_name = "/blog/login.html"), name='login'), 
     path('logout/', view=LogoutView.as_view(template_name = "/blog/logout.html"), name='logout'),
+    path('register/', view=Register.as_view, name='register'),
+    path('profile/', view=Profile.as_view, name='profile' ),
 ]
