49b9b9e702d06f46d19d3b0033af513a7cb4f474
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-19 10:31:56 +0000

    week14_V6

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', view=Home.as_view(), name='home'),
     path('login/', view=LoginView.as_view(template_name = "/blog/login.html"), name='login'), 
     path('logout/', view=LogoutView.as_view(template_name = "/blog/logout.html"), name='logout'),
-    path('register/', view=Register.as_view( template_name = '/blog/register.html'), name='register'),
+    path('register/', view=Register.as_view(), name='register'),
     path('profile/', view=Profile.as_view(), name='profile' ),
     path('post/', view= Post.as_view(), name = 'posts')
 ]
