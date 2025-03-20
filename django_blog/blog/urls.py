39b79c66ca3f0f457ad4050ba797e113e9547afa
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-20 16:11:22 +0000

    week14_V14

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,14 @@ urlpatterns = [
     path('post/new/', view= CreateView.as_view(), name = 'create-posts'),
     path('post/<int:pk>/update/', view= UpdateView.as_view(), name = 'update-posts'),
     path('post/<int:pk>/delete/', view= DeleteView.as_view(), name = 'delete-posts'),
-    path('post/<int:pk>/comments/new',view= CommentCreateView.as_view(), name='create_comment'),
+    path('post/<int:pk>/comments/new/',view= CommentCreateView.as_view(), name='create_comment'),
     path('post/<int:pk>/comments/',view= CommentListView.as_view(), name='list_comments'),
     path('post/<int:pk>/comments/<int:pk>/',view= CommentDetailView.as_view(), name='comment'),
-    path('post/<int:pk>/comments/update/<int:pk>/',view= CommentUpdateView.as_view(), name='update_comments'),
-    path('post/<int:pk>/comments/delete/<int:pk>/',view= CommentDeleteView.as_view(), name='delete_comments'),
+    path('comment/<int:pk>/update/',view= CommentUpdateView.as_view(), name='update_comments'),
+    path('comment/<int:pk>/delete/',view= CommentDeleteView.as_view(), name='delete_comments'),
 
 ]
+
+
+
+#["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
