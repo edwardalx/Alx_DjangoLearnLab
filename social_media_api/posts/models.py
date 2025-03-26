45d647af636332498001ec227c71b27267694f24
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-26 18:34:12 +0000

    social_media_api_02

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    content = models.TimeField(max_length=300, null= True, blank=True)
+    content = models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
