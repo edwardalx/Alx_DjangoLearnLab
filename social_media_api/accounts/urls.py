e16a097d084bf2f55d441794a031625929e1049e
edwardalx <eobkwaku@gmail.com>
2025-03-28 00:34:43 +0000

    social_media_api_03

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import CustomUserRegisterApi, CustomUserLoginAPIView,ProfileManagementView
+from .views import CustomUserRegisterApi, CustomUserLoginAPIView,ProfileManagementView,follow_user,unfollow_user
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -8,5 +8,6 @@ urlpatterns=[
    path('register/', view=CustomUserRegisterApi.as_view(),name='register-api' ),
    path('login/', view=CustomUserLoginAPIView.as_view(), name='login-api'),
    path('', include(router.urls)),
-   
+   path('follow/<int:pk>',view=follow_user , name='folloe-user'),
+   path('unfollow/<int:pk>', view=unfollow_user, name='Unfollow-user'), 
 ]
