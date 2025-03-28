4f001fce7c8f06c125b02267153fee0f0dd863e1
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-03-28 17:14:11 +0000

    social_media_api_04

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -6,6 +6,11 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import api_view,permission_classes
+from models import Like
+from notifications.models import Notification
+from rest_framework.response  import Response
+from rest_framework import status
+from django.contrib.contenttypes.models import ContentType
 
 # Create your views here.
 
@@ -43,11 +48,34 @@ class PostFeedView(generics.ListAPIView):
 #if request.user in Like.user raise error
 #else add post to Like.post
 
+def create_notification(recipient, actor, verb, target_object):
+    """Helper function to create a notification."""
+    Notification.objects.create(
+        recipient=recipient,
+        actor=actor,
+        verb=verb,
+        content_type=ContentType.objects.get_for_model(target_object),
+        object_id=target_object.id
+    )
+
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def like_post(request, post_id):
-    ...
-
+    post_to_like = generics.get_object_or_404(Post, id = post_id)
+    if post_to_like.author == request.user:
+        return Response({'error': 'you can not like your own post'})
+    like, created =Like.objects.get_or_create(user=request.user, post=post_to_like)
+    if like:
+        return Response({'message', 'Post Liked'}, status=status.HTTP_201_CREATED)
+    Response({'error': 'You have already liked this post.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    create_notification(
+            recipient=post_to_like.author, 
+            actor=request.user, 
+            verb=f"{request.user.username} started following you.", 
+            target_object=request.user
+        )
+    return Response({"message": f"You are now following {post_to_like.author.username}"}, status=status.HTTP_200_OK)
 
 
 
