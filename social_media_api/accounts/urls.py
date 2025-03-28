5cf35c7981ce7bd80d5900cb14319db3c0525868
edwardalx <eobkwaku@gmail.com>
2025-03-28 00:41:37 +0000

    social_media_api_03

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
    path('register/', view=CustomUserRegisterApi.as_view(),name='register-api' ),
    path('login/', view=CustomUserLoginAPIView.as_view(), name='login-api'),
    path('', include(router.urls)),
-   path('follow/<int:pk>',view=follow_user , name='folloe-user'),
-   path('unfollow/<int:pk>', view=unfollow_user, name='Unfollow-user'), 
+   path('follow/<int:user_id>',view=follow_user , name='folloe-user'),
+   path('unfollow/<int:user_id>', view=unfollow_user, name='Unfollow-user'), 
   
 ]
